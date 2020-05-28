fc72afc2f7b90cc5117041c386d9e1336b2b0e71
yaroslavkrutiak <yaroslav.krut02@gmail.com>
2020-05-28 15:20:35 +0300

    colors+ nav bar+ new posts button

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import './pages/MainPage.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PetHub',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        backgroundColor: Colors.black12,
+          bottomAppBarColor:Colors.black,
+        primarySwatch: Colors.yellow,
         fontFamily: 'Cinzel'
       ),
       home: MainPage(),
